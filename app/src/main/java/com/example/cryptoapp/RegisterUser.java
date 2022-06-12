6e0d83bbd6992a216c10e9e798b81bbe69a61a7b
teomeskuti <teomeskuti29@gmail.com>
2022-06-12 13:09:01 +0200

    Second commit

--- app/src/main/java/com/example/cryptoapp/RegisterUser.java
+++ app/src/main/java/com/example/cryptoapp/RegisterUser.java
@@ -1,14 +1,115 @@
 package com.example.cryptoapp;
 
+import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText editTextFullName, editTextEmail, editTextPassword;
+    Button register;
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        register = (Button) findViewById(R.id.registerUser);
+        register.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullName);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+
+    }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registerUser:
+                registerUser();
+                break;
+        }
+    }
 
+    private void registerUser() {
+        String name = editTextFullName.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            editTextFullName.setError("Name cannot be empty!");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email cannot be empty!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password cannot be empty!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length()<6){
+            editTextPassword.setError("Password must be at least 6 chara cters!");
+            editTextPassword.requestFocus();
+            return;
+
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            User user = new User(name, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if(task.isSuccessful())
+                                                Toast.makeText(RegisterUser.this, "User registered successfully!", Toast.LENGTH_LONG).show();
+                                            else
+                                                Toast.makeText(RegisterUser.this, "Failed to register user!", Toast.LENGTH_LONG).show();
+
+                                        }
+                                    });
+                        }else
+                            Toast.makeText(RegisterUser.this, "Failed to register user!", Toast.LENGTH_LONG).show();
+
+                    }
+                });
     }
 }
